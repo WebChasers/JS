049a7424e1e26f9aabdc7a4db2ed9b8e592f469b
Nasir arain <116964436+WebChasers@users.noreply.github.com>
2026-01-10 13:34:07 +0000

    complete summary

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -6,3 +6,27 @@
 // object
 // typeof  to know which type of varibale is this 
 // but typeof null is only bug which shows object
+
+// data types can be primitive and non primitive
+//primitive (call by value) copy changes not an address 
+// string, Number, Boolean ,null , undefined , symbol , BigInt(big values )
+
+
+const id = symbol('2');
+const anotherId = symbol('2 ')
+console.log(id === anotherId);  // so in this return values will not be same
+
+// mostly unique things like IDs 
+// Reference (Non-primitive) return types mostly object
+// from memory directly using address can be called
+// arrays , objects , functions 
+const arr = ["Ali", "Ahmed", "Anus"]
+//objects
+let obj = {
+    name:"Nasir",
+    age:22
+}
+
+const my = function arr(){
+    console.log("helloworld");
+}
