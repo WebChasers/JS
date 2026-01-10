644a68fb9ae08a4e3ad015de38ac4d2d14eebb76
Nasir arain <116964436+WebChasers@users.noreply.github.com>
2026-01-10 12:05:11 +0000

    covnerstions

---
+++ 01_basics/conversionsOperations.js
@@ -0,0 +1,19 @@
+let score = "45abc" //it its mixed 
+
+// console.log(typeof (score))
+
+
+let res = Number(score) 
+// console.log(typeof(res))
+// console.log(res) //what it will print NaN ?
+
+//converting in Number
+// so if "50" => number 50
+// if "546abc" => NaN
+// true => 1;  false => 0;
+
+let isLoggedIn = 1
+let boolisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof(boolisLoggedIn))
+
+// 1>true 0>false "">false "srefre">true
