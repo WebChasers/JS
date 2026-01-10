c0e21e30e3249bc67dcb2007bae6cfdd11edc488
Nasir arain <116964436+WebChasers@users.noreply.github.com>
2026-01-10 11:48:43 +0000

    var/types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) using nodejs not browser 
+
+//symbol => for unique
+//number ,string , bigInt , null , boolean
+// object
+// typeof  to know which type of varibale is this 
+// but typeof null is only bug which shows object
---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountID = 123456
+let accountEmail = "Ahmed@gmail.com"
+var accountName = "Varqu"
+
+console.table([accountID , accountName , accountEmail])
+
+/*
+
+*/
