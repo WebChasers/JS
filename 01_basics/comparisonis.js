e48e27fea54ffa32793ed6ea2033444556f67ad5
Nasir arain <116964436+WebChasers@users.noreply.github.com>
2026-01-10 12:44:22 +0000

    complete

---
+++ 01_basics/comparisonis.js
@@ -0,0 +1,19 @@
+// console.log (2>1)
+// console.log(2==1)
+// console.log(2<1)
+// console.log(2!=1)
+// console.log(2>=1)
+
+// console.log("2" > 1) 
+// console.log("02" > 1) 
+//so while comparing make sure datatype is same other wise it will not be an predictable 
+
+// console.log(null > 0);
+// console.log(null == 0); // The Equality Rule (==)
+// When you use the "loose equality" check (==), JavaScript DOES NOT convert null to a number.
+// console.log(null >= 0); //conversion issue so not predictable output 
+// comparsions convert null to a number, treating it as an 0 bu
+// console.log(null == undefined);
+// === strictly check compare not just value but also their datatype
+
+console.log("2" === "1")
