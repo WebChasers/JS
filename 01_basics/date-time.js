9ac7dc7f68349e4d9007db95e360a931fb096afa
Nasir arain <116964436+WebChasers@users.noreply.github.com>
2026-01-13 12:42:40 +0000

    fixedDate

---
+++ 01_basics/date-time.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toJSON());
+console.log(myDate.toISOString());
+
+let newDate = new Date("2023-01-14")
+console.log(newDate.toDateString());
