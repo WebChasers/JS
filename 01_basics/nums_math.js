e0163abd8adede1bd878a6ea55a08d815edc74f6
Nasir arain <116964436+WebChasers@users.noreply.github.com>
2026-01-13 12:26:00 +0000

    updated

--- 01_basics/nums_math.js
+++ 01_basics/nums_math.js
@@ -1,7 +1,23 @@
-const score = 400;
-console.log(typeof(score));
-const balance = new Number(100)
-console.log(balance)
-console.log(balance.toString())
-console.log();
-console.log(typeof(balance))
+// const score = 400;
+// console.log(typeof(score));
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(typeof(balance))
+// console.log(balance.toFixed(2))
+// const nums = 10000000000
+// console.log(nums.toLocaleString())
+// console.log(Math.abs(-3));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.1));
+// console.log(Math.min(2,3,4,5,6,7,8));
+// console.log(Math.max(2,3,4,5,6,7,8));
+
+
+const max = 20 , min = 10
+console.log(Math.floor(Math.random() * (max - min + 1)));
+
+
+
+
+
